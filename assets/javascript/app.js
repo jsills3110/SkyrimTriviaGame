a4a29405f57d2d664acf3dbc43f274f7ca5a6445
Jessica Sills <jsills3110@gmail.com>
2019-09-23 13:13:32 -0700

    Created an object for the TriviaGame with an initial set of questions. Set up the html. Set the background to an image of Skyrim.

---
+++ assets/javascript/app.js
@@ -0,0 +1,62 @@
+var SkyrimTrivia = {
+    questions: [
+        {
+            question: "What were the Falmer known as in ancient times?",
+            answer1: "Cave Dwellers",
+            answer2: "Skyrim Elves",
+            answer3: "People of Mereth",
+            correct: "Snow Elves",
+        },
+        {
+            question: "Who was the first human to arrive in Skyrim?",
+            answer1: "Jeek of the River",
+            answer2: "Kyne",
+            answer3: "Yngol",
+            correct: "Ysgramor",
+        },
+        {
+            question: "Who is someone you can encounter on the roads of Skyrim, as well as in other Elder Scrolls games?",
+            answer1: "Plautis and Salonia Carvain",
+            answer2: "Balbus",
+            answer3: "Sond and Bottar",
+            correct: "M'aiq the Liar",
+        },
+        {
+            question: "Who is the leader of the Stormcloak Rebellion?",
+            answer1: "Hoag Stormcloak",
+            answer2: "High King Torygg",
+            answer3: "Galmar Stone-Fist",
+            correct: "Ulfric Stormcloak",
+        },
+        {
+            question: "When was the last time that the Greybeards spoke?",
+            answer1: "When their founder, Jurgen Windcaller, was defeated at Red Mountain",
+            answer2: "When they defeated the Dragon God of Time, Akatosh",
+            answer3: "When they settled their seat of power, High Hrothgar",
+            correct: "When they announced the greatness of Tiber Septim I",
+        },
+        {
+            question: "Why does Anise, the sweet old Nord lady, attack you?",
+            answer1: "Because you failed her quest, in which you send her purple mountain flowers for her birthday",
+            answer2: "Because you killed her grandson, Hern",
+            answer3: "Because you told her her sweet rolls weren't good",
+            correct: "Because you stole from her or entered her cellar",
+        },
+        {
+            question: "If you follow the road to Riverwood after escaping Helgen, what are the three Standing Stones you encounter?",
+            answer1: "The Lord, The Lady, and The Steed",
+            answer2: "The Shadow, The Warrior, and The Apprentice",
+            answer3: "The Atronach, The Thief, and The Ritual",
+            correct: "The Thief, The Warrior, and The Mage",
+        },
+        {
+            question: "What are guards most famous for saying?",
+            answer1: "My cousin's out fighting dragons, and what do I get? Guard duty.",
+            answer2: "No lollygaggin'.",
+            answer3: "I'd be a lot warmer and a lot happier with a bellyful of mead...",
+            correct: "I used to be an adventurer like you, then I took an arrow to the knee.",
+        },
+    ],
+
+    
+};
